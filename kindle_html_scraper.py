d135372481145639241582dde101440067bb86a7
Matthew Woo <matthewwoo@Matthews-MacBook-Pro.local>
2020-04-23 07:43:38 -0700

    Stored all highlights and notes into seperate lists

--- kindle_html_scraper.py
+++ kindle_html_scraper.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
-html_doc = open('/Users/matthewwoo/Desktop/Kindle_Anki_Script/Story10x.html')
+import pandas as pd
+
+# Open the file and store all highlight in a list
+html_doc = open('/Users/matthewwoo/Desktop/Kindle_Anki/Story10x.html')
 soup = BeautifulSoup(html_doc,'html.parser')
 noteTexts = soup.find_all('div',{'class':'noteText'})
 noteTexts_list = []
@@ -7,6 +10,25 @@ for note in noteTexts:
     noteTexts_list.append(note)
 print(noteTexts_list[0])
 print(noteTexts_list[1])
+
+# Store all the highlights and notes into separate list
+highlights = []
+notes = []
+
+for i in range(0, len(noteTexts)):
+    if i % 2: 
+        highlights.append(noteTexts[i])
+    else: 
+        notes.append(noteTexts[i])
+print("## Highlights")
+print(highlights)
+
+print("## Notes")
+print(notes)
+
+## Input the highlights & notes into the pandas dataframe 
+
+
 
 
 
